ca5b14f1f887676549165c371c01be7bb6431efa
sole <listas@soledadpenades.com>
2014-12-27 18:19:30 +0100

    move to async, and pseudodetect html imports

--- WebComponentsSupport.js
+++ WebComponentsSupport.js
@@ -1,24 +1,89 @@
 (function() {
 
-	// custom elements -> registerElement
-	var regElemAvailable = !!document.registerElement;
-	var regElemNative = false;
-	if(regElemAvailable) {
-		regElemNative = isNativeCode(document.registerElement);
-	}
+	function detectSupport(doneCallback) {
 
-	// shadowDom -> createShadowRoot
-	var dummyDiv = document.createElement('div');
-	var shadowRootAvailable = !!dummyDiv.createShadowRoot;
-	var shadowRootNative = false;
-	if(shadowRootAvailable) {
-		shadowRootNative = isNativeCode(dummyDiv.createShadowRoot);
-	}
+		// custom elements -> registerElement
+		var regElemAvailable = !!document.registerElement;
+		var regElemNative = false;
+		if(regElemAvailable) {
+			regElemNative = isNativeCode(document.registerElement);
+		}
 
-	// html imports
-	// html templates
-	var dummyTemplate = document.createElement('template');
-	var templatesAvailable = !!(dummyTemplate.content);
+		// shadowDom -> createShadowRoot
+		var dummyDiv = document.createElement('div');
+		var shadowRootAvailable = !!dummyDiv.createShadowRoot;
+		var shadowRootNative = false;
+		if(shadowRootAvailable) {
+			shadowRootNative = isNativeCode(dummyDiv.createShadowRoot);
+		}
+
+		// html templates
+		var dummyTemplate = document.createElement('template');
+		var templatesAvailable = !!(dummyTemplate.content);
+
+		// html imports
+		var IMPORT_LINK_TYPE = 'import';
+		var importsMaybeAvailable = Boolean(IMPORT_LINK_TYPE in document.createElement('link'));
+		var importsAvailable = false;
+		var detectionTimeout = null;
+
+		// Let's run a quick test and see if imports are actually loading anything at all.
+		if(importsMaybeAvailable) {
+			var dummyLink = document.createElement('link');
+
+			var testStr = 'Supercalifragilisticexpialidocious';
+			var testBlob = new Blob([testStr], { type: 'text/html' });
+						
+			dummyLink.rel = 'import';
+			dummyLink.onload = function() {
+
+				var importBody = dummyLink.import.body;
+				if(importBody && importBody.innerHTML.search(testStr) === 0) {
+					importsAvailable = true;
+				}
+
+				// Always clean up after yourself!
+				removeDummyLink();
+
+				completeDetection();
+			};
+			dummyLink.href = URL.createObjectURL(testBlob);
+			
+			// apparently doesn't start loading until appended to <head>
+			document.head.appendChild(dummyLink);
+
+			// Give it some time for browsers that won't load anything actually,
+			// so we can complete the test
+			detectionTimeout = window.setTimeout(completeDetection, 25);
+		} else {
+			// No, not available at all!
+			completeDetection();
+		}
+
+		function removeDummyLink() {
+			document.head.removeChild(dummyLink);
+		}
+		
+		function completeDetection() {
+			
+			if(detectionTimeout) {
+				clearTimeout(detectionTimeout);
+				detectionTimeout = null;
+			}
+
+			var results = {
+				customElements: makeSupportObject(regElemAvailable, regElemNative),
+				shadowDOM: makeSupportObject(shadowRootAvailable, shadowRootNative),
+				// assuming if templates are available, support is native
+				// - not sure how to test this as I can't access a browser without template support
+				templates: makeSupportObject(templatesAvailable, templatesAvailable),
+				htmlImports: makeSupportObject(importsAvailable, importsAvailable)
+			};
+
+			doneCallback(results);
+		}
+
+	}
 
 	function isNativeCode(fun) {
 		var src = fun.toString();
@@ -37,14 +102,9 @@
 		return obj;
 	}
 
-	var WebComponentsSupport = {
-		customElements: makeSupportObject(regElemAvailable, regElemNative),
-		shadowDOM: makeSupportObject(shadowRootAvailable, shadowRootNative),
-		// assuming if templates are available, support is nativ
-		// - not sure how to test this as I can't access a browser without template support
-		templates: makeSupportObject(templatesAvailable, templatesAvailable)
-	};
+	var WebComponentsSupport = detectSupport;
 
+	
 	// Make it compatible for require.js/AMD loader(s)
 	if(typeof define === 'function' && define.amd) {
 		define(function() { return WebComponentsSupport; });
